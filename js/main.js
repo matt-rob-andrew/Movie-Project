c0599bb4207a22af44dc63e896841eb1200a13eb
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-09 12:11:55 -0600

    added /movies to url

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 // Glitch url
-let url = 'https://stellar-trusting-death.glitch.me'
+let url = 'https://stellar-trusting-death.glitch.me/movies'
 fetch(url)
     .then(response => console.log(response))
     
