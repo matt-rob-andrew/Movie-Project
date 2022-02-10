3a6c2e76a25dc378c5879aa2735a3ad0df9aa778
Andrew Lange <andrew.lange13@gmail.com>
2022-02-10 09:56:54 -0600

    adds for loop to loop through movie data

--- js/main.js
+++ js/main.js
@@ -1,6 +1,25 @@
 "use strict";
 // Glitch url
-let url = 'https://stellar-trusting-death.glitch.me/movies'
-fetch(url)
-    .then(response=>response.json())
-    .then(data => console.log(data))
+
+function moviePage() {
+    let url = 'https://stellar-trusting-death.glitch.me/movies'
+    return fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .then(data => {
+            let card = ""
+            for (let i = 0; i < data.length; i++) {
+                let title = data[i].title;
+                let rating = data[i].rating;
+                let director = data[i].director;
+                let poster = data[i].poster;
+                let actors = data[i].actors
+
+                card += ``
+
+
+            }
+        })
+}
+
+console.log(moviePage());
