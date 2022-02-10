8a0c7aad3e2b3ecc57af120f2d5b1e92bafc6647
Matthew Lewis <matthew.d.lewis64@gmail.com>
2022-02-10 10:58:02 -0600

    append movie cards to html, bootstrap styling

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 "use strict";
+
 // Glitch url
 
 function moviePage() {
     let url = 'https://stellar-trusting-death.glitch.me/movies'
     return fetch(url)
         .then(response => response.json())
-        .then(data => console.log(data))
+        // .then(data => console.log(data))
         .then(data => {
             let card = ""
             for (let i = 0; i < data.length; i++) {
@@ -15,11 +16,24 @@ function moviePage() {
                 let poster = data[i].poster;
                 let actors = data[i].actors
 
-                card += ``
-
-
+                card += `
+                <div class="col">
+                <div class="card">
+                  <img src="${poster}" class="card-img-top" alt="...">
+                  <div class="card-body">
+                    <h5 class="card-title text-capitalize">${title}</h5>
+                    <p class="card-text">Rating: ${rating}</p>
+                    <p class="card-text">Directed by: ${director}</p>
+                    <p class="card-text">Starring: ${actors}</p>
+                  </div>          
+                </div>                  
+                </div>
+                `
+                
             }
+            // Append cards to main container
+            $('#movie-container').html(card)
         })
 }
-
+moviePage()
 console.log(moviePage());
